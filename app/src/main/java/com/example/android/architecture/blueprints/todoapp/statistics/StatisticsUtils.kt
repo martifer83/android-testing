1a5681aca3d67e7f94d1d4989f05ba5fe110aac6
martifer <kismadar17@gmail.com>
2021-02-01 18:40:56 +0100

    afegits testos per fer detectar errades

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtils.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtils.kt
@@ -22,12 +22,17 @@ import com.example.android.architecture.blueprints.todoapp.data.Task
  * Function that does some trivial computation. Used to showcase unit tests.
  */
 internal fun getActiveAndCompletedStats(tasks: List<Task>?): StatsResult {
+
     val totalTasks = tasks!!.size
     val numberOfActiveTasks = tasks.count { it.isActive }
+    val activePercent = 100 * numberOfActiveTasks / totalTasks
+    val completePercent = 100 * (totalTasks - numberOfActiveTasks) / totalTasks
+
     return StatsResult(
-        activeTasksPercent = 100f * numberOfActiveTasks / tasks.size,
-        completedTasksPercent = 100f * (totalTasks - numberOfActiveTasks) / tasks.size
+            activeTasksPercent = activePercent.toFloat(),
+            completedTasksPercent = completePercent.toFloat()
     )
+
 }
 
 data class StatsResult(val activeTasksPercent: Float, val completedTasksPercent: Float)
