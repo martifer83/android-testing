a916c5a81ab80b51a089a84c6fceffdeaf4ce58b
martifer <kismadar17@gmail.com>
2021-02-01 18:44:24 +0100

    afegida solucio per pasar tests

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtils.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtils.kt
@@ -23,16 +23,16 @@ import com.example.android.architecture.blueprints.todoapp.data.Task
  */
 internal fun getActiveAndCompletedStats(tasks: List<Task>?): StatsResult {
 
-    val totalTasks = tasks!!.size
-    val numberOfActiveTasks = tasks.count { it.isActive }
-    val activePercent = 100 * numberOfActiveTasks / totalTasks
-    val completePercent = 100 * (totalTasks - numberOfActiveTasks) / totalTasks
-
-    return StatsResult(
-            activeTasksPercent = activePercent.toFloat(),
-            completedTasksPercent = completePercent.toFloat()
-    )
-
+    return if (tasks == null || tasks.isEmpty()) {
+        StatsResult(0f, 0f)
+    } else {
+        val totalTasks = tasks.size
+        val numberOfActiveTasks = tasks.count { it.isActive }
+        StatsResult(
+                activeTasksPercent = 100f * numberOfActiveTasks / tasks.size,
+                completedTasksPercent = 100f * (totalTasks - numberOfActiveTasks) / tasks.size
+        )
+    }
 }
 
 data class StatsResult(val activeTasksPercent: Float, val completedTasksPercent: Float)
